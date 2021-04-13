337b455502ed192b7f9b908279a623a5ad95d1ed
大野勇気 <couragetest23@gmail.com>
2021-04-13 19:17:00 +0900

    [create] Admin Seeder

---
+++ database/seeders/AdminsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+            'name' => config('admin.name'),
+            'email' => config('admin.email'),
+            'password' => Hash::make(config('admin.password')),
+        ]);
+    }
+}
