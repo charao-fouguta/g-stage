0392d16a9e63b5faa57a00ab072555ef6109f651
大野勇気 <couragetest23@gmail.com>
2021-05-12 12:41:33 +0900

    [change] Google Analytics

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,12 +11,12 @@
 
 	@if(config('app.env') === 'production')
 		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y77YF5XSZL"></script>
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-C4ZMQ9QT3B"></script>
 		<script>
 		window.dataLayer = window.dataLayer || [];
 		function gtag(){dataLayer.push(arguments);}
 		gtag('js', new Date());
-		gtag('config', 'G-Y77YF5XSZL');
+		gtag('config', 'G-C4ZMQ9QT3B');
 		</script>
 	@endif
 	<script>document.createElement( "picture" );</script>
